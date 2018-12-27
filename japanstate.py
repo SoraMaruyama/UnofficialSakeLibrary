90b6276184cfa707fbf47902a8b02e4ecd8e526d
Sakura <sorahonda2@gmail.com>
2018-12-27 15:53:29 +0900

    remove a file

--- japanstate.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-def JapanState():
-    states = [
-
-        {"en":"Tokyo", "jpn":""},
-        {"en":"Osaka", "jpn":""},
-        {"en":"Fukuoka", "jpn":""}
-    ]
-    return states
--- usertype.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-def UserType():
-    usertypes = [
-
-        {"en":"Sake Consumer", "jpn":""},
-        {"en":"Sake Buyer", "jpn":""},
-        {"en":"Sake Maker", "jpn":""}
-    ]
-    return usertypes
