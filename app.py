2c04f4248a396c12e1a570a56297c849c1ba4069
Sakura <sorahonda2@gmail.com>
2019-01-01 11:55:36 +0900

    commeted out firebase auth code

--- app.py
+++ app.py
@@ -23,23 +23,20 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred, {
   'projectId': 'sora-sakelibrary',
-  'databaseAuthVariableOverride': {
-        'uid': 'my-service-worker'
-    }
 })
 
 db = firestore.client()
 
 
-user = auth.create_user(
-    email='user@example.com',
-    email_verified=False,
-    phone_number='+15555550100',
-    password='secretPassword',
-    display_name='John Doe',
-    photo_url='http://www.example.com/12345678/photo.png',
-    disabled=False)
-print('Sucessfully created new user: {0}'.format(user.uid))
+# user = auth.create_user(
+#     email='user@example.com',
+#     email_verified=False,
+#     phone_number='+15555550100',
+#     password='secretPassword',
+#     display_name='John Doe',
+#     photo_url='http://www.example.com/12345678/photo.png',
+#     disabled=False)
+# print('Sucessfully created new user: {0}'.format(user.uid))
 
 client = vision.ImageAnnotatorClient()
 
