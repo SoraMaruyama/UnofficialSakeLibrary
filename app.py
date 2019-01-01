ccc05042311a5cd7f236337a25b545ff94c1b41d
Sakura <sorahonda2@gmail.com>
2019-01-01 11:47:59 +0900

    create feedback sake maker now on firebase

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 import os
 import io
 import json
-import firebase_admin
-from firebase_admin import db, credentials
 from flask import Flask, render_template, g, flash, request, redirect, url_for, send_from_directory, session, jsonify, abort, make_response
 from random import randint
 from time import strftime
@@ -11,21 +9,37 @@ from wtforms import Form, TextField, FileField, TextAreaField, validators, Strin
 from flask_dropzone import Dropzone
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from data import Sakes
-from japanstate import JapanState
-from usertype import UserType
+
 from werkzeug import utils
 from google.cloud import vision
 
-firebase_admin.initialize_app(options={
-    'databaseURL': 'https://sora-sakelibrary.firebaseio.com'
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import auth
+from flask_login import LoginManager, UserMixin, login_user, logout_user
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': 'sora-sakelibrary',
+  'databaseAuthVariableOverride': {
+        'uid': 'my-service-worker'
+    }
 })
-# cred = credentials.Certificate("./firebase.json")
-# firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 
-SakesFirebase = db.reference("sakes")
-MakersFirebase = db.reference("makers")
-UsersFirebase = db.reference("users")
+user = auth.create_user(
+    email='user@example.com',
+    email_verified=False,
+    phone_number='+15555550100',
+    password='secretPassword',
+    display_name='John Doe',
+    photo_url='http://www.example.com/12345678/photo.png',
+    disabled=False)
+print('Sucessfully created new user: {0}'.format(user.uid))
 
 client = vision.ImageAnnotatorClient()
 
@@ -52,14 +66,8 @@ app.config['MAX_CONTENT_LENGTH'] = 1 * 2024 * 2024
 app.config['SECRET_KEY'] = os.urandom(24)
 
 Sakes = Sakes()
-JapanState = JapanState()
-UserType = UserType()
 
-# Dropzone settings
-# app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
-# app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
-# app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
-# app.config['DROPZONE_REDIRECT_VIEW'] = 'upload'
+
 
 # Uploads settings
 app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + 'static/img'
@@ -69,6 +77,8 @@ configure_uploads(app, photos)
 patch_request_class(app)  # set maximum file size, default is 16MB
 latestImgPath = []
 imageFileName = []
+
+
 
 
 
@@ -137,20 +147,18 @@ class SignUpForm(Form):
     firstname = TextField('Firstname:', validators=[validators.required()])
     lastname = TextField(':Lastname:', validators=[validators.required()])
     usertype = SelectField('Usertype:', choices=[('sake_consumer','Sake Consumer'),('sake_buyer','Sake Buyer'),('sake_maker','Sake Maker'), ('others','Others')], validators=[validators.required()])
-    email = StringField("Email:",  validators=[validators.required("Please enter your email address."), validators.Email("This field requires a valid email address")])
+    email = StringField("Email:",  validators=[validators.required("Please enter your email address.")])
     password = PasswordField("Password:",validators=[validators.DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[validators.EqualTo('password')])
-    # email = TextField('Email:', validators=[validators.required()])
 
 def get_time_userAdded():
     time = strftime("%Y-%m-%dT%H:%M")
     return time
 
-def write_user_log(firstname, lastname, usertype, email):
-    data = open('logs/users.log', 'a')
-    timestamp = get_time_userAdded()
-    data.write('DateStamp={}, Firstname={}, Lastname={}, Usertype={}, Email={} \n'.format(timestamp, firstname, lastname, usertype, email))
-    data.close()
+def write_user_log(firstname, lastname, usertype, email, password):
+    email = 'sora@gmail.com'
+    password = '2kazuhokai'
+    print(user)
+
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
@@ -162,18 +170,18 @@ def signup():
         usertype=request.form['usertype']
         email=request.form['email']
         password=request.form['password']
-        password2=request.form['password2']
         
 
         if form.validate():
-            write_user_log(firstname, lastname, usertype, email)
+            write_user_log(firstname, lastname, usertype, email, password)
             flash('Hello: {} {}'.format(firstname, lastname))
             return render_template('home.html')
 
         else:
             flash('Error: All Fields are Required')
         
-    return render_template('signup.html', form=form, usertypelist = UserType)
+    return render_template('signup.html', form=form)
+
 
 
 class AddSakeForm(Form):
@@ -186,18 +194,17 @@ def get_time_addsake():
     time = strftime("%Y-%m-%dT%H:%M")
     return time
 
-def write_to_sakelist(request, name, saketype, taste, description):
-    data = open('logs/addsake.log', 'a')
-    with open('addsake.json', 'w') as f:
-        json.dump(request.form, f)
+def add_sake_to_db(name, saketype, taste, description):
     timestamp = get_time_addsake()
-    data.write('DateStamp={}, Name={}, Saketype={}, Taste={}, Description={} \n'.format(timestamp, name, saketype, taste, description))
-    data.close()
-    # sakedata = request.get_json()
-    # print(sakedata)
-    # req = sakedata
-    # sake = SakesFirebase.push(req)
-    # return jsonify({'id': sake.key}), 201
+    db.collection('sakes').document(name).set({
+        'createdAt': timestamp,
+        'name': name,
+        'saketype': saketype,
+        'taste': taste,
+        'Description': description,
+    })
+
+
 
 @app.route("/addSake", methods=['GET', 'POST'])
 def addsake():
@@ -210,7 +217,7 @@ def addsake():
         description=request.form['description']
 
         if form.validate():
-            write_to_sakelist(request, name, saketype, taste, description)
+            add_sake_to_db(name, saketype, taste, description)
             flash('Sake Added!: {} {}'.format(name, saketype))
 
         else:
@@ -222,15 +229,14 @@ class FeedbackForm(Form):
     title = TextField('Title:', validators=[validators.required()])
     feedback = TextAreaField('Feedback:', validators=[validators.required()])
 
-def get_time_feedback():
-    time = strftime("%Y-%m-%dT%H:%M")
-    return time
 
 def write_to_feedback(title, feedback):
-    data = open('logs/feedback.log', 'a')
-    timestamp = get_time_feedback()
-    data.write('DateStamp={}, Title={}, Feedback={} \n'.format(timestamp, title, feedback))
-    data.close()
+    timestamp = strftime("%Y-%m-%dT%H:%M")
+    db.collection('feedbacks').document(title).set({
+        'createdAt': timestamp,
+        'title': title,
+        'feedback': feedback,
+    })
 
 @app.route("/feedback", methods=['GET', 'POST'])
 def feedback():
@@ -255,15 +261,16 @@ class AddMakerForm(Form):
     region = SelectField('Region:', choices=[('tokyo','Tokyo'),('osaka','Osaka'),('fukuoka','Fukuoka'),('niigata','Niigata')])
     url = TextField('Url:', validators=[validators.url(require_tld=True, message="Invalid URL")])
 
-def get_time_addmaker():
-    time = strftime("%Y-%m-%dT%H:%M")
-    return time
 
 def write_to_makerlist(maker, region, url):
-    data = open('logs/makers.log', 'a')
-    timestamp = get_time_addmaker()
-    data.write('DateStamp={}, Maker={}, Region={}, Url={} \n'.format(timestamp, maker, region, url))
-    data.close()
+    timestamp = strftime("%Y-%m-%dT%H:%M")
+    db.collection('makers').document(maker).set({
+        'createdAt': timestamp,
+        'maker': maker,
+        'region': region,
+        'url': url,
+    })
+
 
 @app.route("/addMaker", methods=['GET', 'POST'])
 def addmaker():
@@ -281,7 +288,7 @@ def addmaker():
         else:
             flash('Error: All Fields are Required')
         
-    return render_template('addMaker.html', form=form, japanstateList = JapanState)
+    return render_template('addMaker.html', form=form)
 
 
 #API
@@ -333,12 +340,7 @@ def about():
 def sakelist():
     return render_template("sakelist.html", sakes = Sakes)
 
-@app.route("/sakelistall")
-def sakelistall():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT,"addsake.json")
-    data = json.load(open(json_url))
-    return render_template('sakelistall.html', data=data)
+
 
 if __name__ == '__main__':
     app.run()
